2839c8fb59debdbd001ebda5c328a57e0a195096
Yunqiao Yang <yunqiaoyang@Yunqiaos-MacBook-Pro.local>
2018-10-24 20:00:17 -0400

    first code submission

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #pages controller, home action
+  root 'pages#home'
+
+  # pages controller, about action
+  get 'about', to: 'pages#about'
+
+end
