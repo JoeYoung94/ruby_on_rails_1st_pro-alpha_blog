38f47dc4fff18ca8602fd51f1a843e103978a256
Yunqiao Yang <yunqiaoyang@Yunqiaos-MacBook-Pro.local>
2018-10-29 08:12:33 -0400

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: {case_sensitive: false },
+            length: { minimum: 3, maximum: 25}
 
+  VALID_EMAIL_REGEX= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+  validates :email, presence: true, length: {maximum: 105},
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_EMAIL_REGEX}
 end
