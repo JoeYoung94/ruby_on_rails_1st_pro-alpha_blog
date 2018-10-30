b24fef5fea44ddf549ee50fbaf5df73b51d6d2e9
Yunqiao Yang <yunqiaoyang@Yunqiaos-MacBook-Pro.local>
2018-10-29 20:18:03 -0400

    add association between user and articles, and debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
+  belongs_to :user
   # add validations
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
+
+  #add validations for user & articles
+  validates :user_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_023857) do
+ActiveRecord::Schema.define(version: 2018_10_29_235237) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
