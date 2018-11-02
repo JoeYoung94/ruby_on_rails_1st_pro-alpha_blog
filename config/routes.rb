dc175f06a62d4037ce9a903ded8ca9b077b6bd85
Yunqiao Yang <yunqiaoyang@Yunqiaos-MacBook-Pro.local>
2018-11-02 08:24:01 -0400

    create login form

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
 
   resources :users, except: [:new]
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
