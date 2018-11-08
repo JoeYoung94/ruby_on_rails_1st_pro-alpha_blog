e22940cb0363862ea748b332f5c16040fe44047c
Yunqiao Yang <yunqiaoyang@Yunqiaos-MacBook-Pro.local>
2018-11-07 21:23:42 -0500

    ..

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,5 +10,4 @@ end
 describe Article do
   it {is_expected.to validate_presence_of(:title)}
   it {is_expected.to validate_presence_of(:description)}
-  it {is_expected.to validate_presence_of(:user_id)}
 end
